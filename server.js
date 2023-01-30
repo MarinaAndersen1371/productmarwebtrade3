2282b13ea198f47767503a9f44f4cbb580705fbb
MarinaAndersen1371 <72156348+MarinaAndersen1371@users.noreply.github.com>
2023-01-30 21:54:34 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const path = require("path");
 const express = require("express");
-const morgan = require("morgan");
 const dotenv = require("dotenv");
 const colors = require("colors");
 const connectDB = require("./config/db.js");
@@ -17,9 +16,6 @@ connectDB();
 
 const app = express();
 
-if (process.env.NODE_ENV === "development") {
-  app.use(morgan("dev"));
-}
 
 app.use(express.json());
 
